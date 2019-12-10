b5b57dd13acddee20c82706525b38f2b76afce6c
Alex Vancina <avancina@helenplum.org>
2019-12-10 09:22:23 -0600

    Remove -t parameter for psql commands

--- shoutbomb.sh
+++ shoutbomb.sh
@@ -8,11 +8,11 @@ date=$(date '+%Y%m%d')
 
 # Replace the hostname and username below as appropriate for your institution.
 # The password for is saved in the .pgpass password file. See readme.md for more information.
-psql -h sierra-db.helenplum.org -p 1032 -d iii -U shoutbomb -t -A -f ./scripts/holds.sql -o ./holds/holds_$date.txt
-psql -h sierra-db.helenplum.org -p 1032 -d iii -U shoutbomb -t -A -f ./scripts/overdues.sql -o ./overdues/overdues_$date.txt
-psql -h sierra-db.helenplum.org -p 1032 -d iii -U shoutbomb -t -A -f ./scripts/renewals.sql -o ./renewals/renewals_$date.txt
-psql -h sierra-db.helenplum.org -p 1032 -d iii -U shoutbomb -t -A -f ./scripts/text_patrons.sql -o ./text_patrons/text_patrons_$date.txt
-psql -h sierra-db.helenplum.org -p 1032 -d iii -U shoutbomb -t -A -f ./scripts/voice_patrons.sql -o ./voice_patrons/voice_patrons_$date.txt
+psql -h sierra-db.helenplum.org -p 1032 -d iii -U shoutbomb -A -f ./scripts/holds.sql -o ./holds/holds_$date.txt
+psql -h sierra-db.helenplum.org -p 1032 -d iii -U shoutbomb -A -f ./scripts/overdues.sql -o ./overdues/overdues_$date.txt
+psql -h sierra-db.helenplum.org -p 1032 -d iii -U shoutbomb -A -f ./scripts/renewals.sql -o ./renewals/renewals_$date.txt
+psql -h sierra-db.helenplum.org -p 1032 -d iii -U shoutbomb -A -f ./scripts/text_patrons.sql -o ./text_patrons/text_patrons_$date.txt
+psql -h sierra-db.helenplum.org -p 1032 -d iii -U shoutbomb -A -f ./scripts/voice_patrons.sql -o ./voice_patrons/voice_patrons_$date.txt
 
 
 # This script uses an lftp bookmark names ShoutbombFTP with a saved username and password.
